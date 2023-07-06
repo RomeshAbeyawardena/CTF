7da085a9e1aa51b8cd757384c44fd48350115bb2
romesh-abeyawardena <romesh.abeyawardena@bellrockgroup.co.uk>
2023-07-06 13:04:14 +0100

    Added migrations

---
+++ CTF.Api/Migrations/20230706120320_Initial.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CTF.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Session",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OwnerTransactionId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Key = table.Column<string>(type: "NVARCHAR(255)", nullable: false),
+                    Token = table.Column<string>(type: "NVARCHAR(800)", nullable: false),
+                    Subject = table.Column<string>(type: "NVARCHAR(255)", nullable: false),
+                    ValidFrom = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    ValidTo = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Session", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transaction",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TransactionTypeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TransactionDefinitionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ParentTransactionId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    GeneratedBySessionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProcessedBySessionId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Payload = table.Column<string>(type: "NVARCHAR(MAX)", nullable: false),
+                    Hash = table.Column<string>(type: "NVARCHAR(2000)", nullable: false),
+                    ValidFrom = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    ProcessedTimestamp = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transaction", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionDefinition",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Key = table.Column<string>(type: "NVARCHAR(255)", nullable: false),
+                    Token = table.Column<string>(type: "NVARCHAR(800)", nullable: false),
+                    Subject = table.Column<string>(type: "NVARCHAR(255)", nullable: false),
+                    Payload = table.Column<string>(type: "NVARCHAR(MAX)", nullable: false),
+                    ValidFrom = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    ValidTo = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionDefinition", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR(80)", nullable: false),
+                    DisplayName = table.Column<string>(type: "NVARCHAR(2000)", nullable: true),
+                    Enabled = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionType", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Session");
+
+            migrationBuilder.DropTable(
+                name: "Transaction");
+
+            migrationBuilder.DropTable(
+                name: "TransactionDefinition");
+
+            migrationBuilder.DropTable(
+                name: "TransactionType");
+        }
+    }
+}
