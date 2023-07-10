daea5e5abd72184061c3d56eb3fb650370f1d0db
romesh-abeyawardena <romesh.abeyawardena@bellrockgroup.co.uk>
2023-07-10 13:43:43 +0100

    Added handlers

---
+++ CTF/Features/Resource/IQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace CTF.Features.Resource;
+
+public interface IQuery
+{
+    string? NameSearch { get; set; }
+    bool? ShowAll { get; set; }
+}
---
+++ CTF/Features/Resource/SaveHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MediatR;
+using RST.DependencyInjection.Extensions.Attributes;
+using RST.Mediatr.Extensions;
+
+namespace CTF.Features.Resource;
+
+public class SaveHandler : RepositoryHandlerBase<SaveCommand, Models.Resource, Models.Resource>
+{
+    [Inject] protected IMediator? Mediator { get; set; }
+    [Inject] protected IMapper? Mapper { get; set; }
+    public SaveHandler(IServiceProvider serviceProvider) : base(serviceProvider)
+    {
+    }
+
+    public override async Task<Models.Resource> Handle(SaveCommand request, CancellationToken cancellationToken)
+    {
+        return await ProcessSave(request, Mapper!.Map<Models.Resource>, cancellationToken);
+    }
+}
